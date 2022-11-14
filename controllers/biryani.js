15027f9a6de0726a167368f9eec29b2208c780ad
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-11-14 12:16:52 -0600

    new puf for delete is created

--- controllers/biryani.js
+++ controllers/biryani.js
@@ -153,3 +153,17 @@ exports.biryani_update_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle a delete one view with id from query
+exports.biryani_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Biryani.findById(req.query.id)
+    res.render('biryanidelete', { title: 'Biryani Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/biryani.js
+++ routes/biryani.js
@@ -14,4 +14,7 @@ router.get('/create', biryani_controlers.biryani_create_Page);
 /* GET create update page */
 router.get('/update', biryani_controlers.biryani_update_Page);
 
+/* GET delete costume page */
+router.get('/delete', biryani_controlers.biryani_delete_Page);
+
 module.exports = router;
