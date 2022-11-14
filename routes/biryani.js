f16b0ed32222071cc3b5cd727c650722a37941c9
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-11-14 11:24:42 -0600

    new pug for create is done

--- routes/biryani.js
+++ routes/biryani.js
@@ -8,4 +8,7 @@ router.get('/', biryani_controlers.biryani_view_all_Page);
 /* GET detail biryani page */
 router.get('/detail', biryani_controlers.biryani_view_one_Page);
 
+/* GET create biryani page */
+router.get('/create', biryani_controlers.biryani_create_Page);
+
 module.exports = router;
