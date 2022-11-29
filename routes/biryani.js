deb84bf95eef5e5e2b5be21f07c1d0ecde901874
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-11-29 14:46:22 -0600

    New files added

--- routes/biryani.js
+++ routes/biryani.js
@@ -1,6 +1,16 @@
 var express = require('express');
-const biryani_controlers= require('../controllers/biryani');
+const biryani_controlers = require('../controllers/biryani');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue onor
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', biryani_controlers.biryani_view_all_Page);
@@ -12,7 +22,7 @@ router.get('/detail', biryani_controlers.biryani_view_one_Page);
 router.get('/create', biryani_controlers.biryani_create_Page);
 
 /* GET create update page */
-router.get('/update', biryani_controlers.biryani_update_Page);
+router.get('/update', secured, biryani_controlers.biryani_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', biryani_controlers.biryani_delete_Page);
