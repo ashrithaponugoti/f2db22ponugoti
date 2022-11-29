83a8c1d175ddf6b44e81c77a6ea00cde63b9e966
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2022-11-29 15:26:13 -0600

    bonus done

--- models/biryani.js
+++ models/biryani.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const biryaniSchema = mongoose.Schema({
     bir_quantity: String,
-    bir_cost: Number,
-    bir_ing: String
+    bir_cost: {type:Number,min:10,max:50},
+    bir_ing: {type:String,maxLength:10}
 })
 module.exports = mongoose.model("Biryani", biryaniSchema)
 
--- routes/biryani.js
+++ routes/biryani.js
@@ -19,12 +19,12 @@ router.get('/', biryani_controlers.biryani_view_all_Page);
 router.get('/detail', biryani_controlers.biryani_view_one_Page);
 
 /* GET create biryani page */
-router.get('/create', biryani_controlers.biryani_create_Page);
+router.get('/create',secured, biryani_controlers.biryani_create_Page);
 
 /* GET create update page */
 router.get('/update', secured, biryani_controlers.biryani_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', biryani_controlers.biryani_delete_Page);
+router.get('/delete', secured, biryani_controlers.biryani_delete_Page);
 
 module.exports = router;
